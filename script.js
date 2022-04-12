82c47a13777c7df4a2eb59d29dad00b97dfe58a3
theslay03 <Kirillpernitsky03@gmail.com>
2022-04-12 10:41:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     var max_width = 375;
+//     $(window).on('resize', function() {
+//       var new_width = $(window).width();
+//     //   var container = $('.container');
+//       if (new_width <= max_width ) {
+//        // container.addClass('compact');
+//         $('.cookie').insertAfter($('.mission'));
+//       }
+//       if (new_width > max_width ) {
+//        // container.removeClass('compact');
+//         $('.sidebar').insertBefore($('.two-box'));
+//       }
+//     }).trigger('resize'); 
+
+//   });
+
+
+
